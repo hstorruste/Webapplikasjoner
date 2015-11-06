8d50bddf818cbf1772e9f9575430433d28239ad9
Adrian Westlund <adrianwestlund@hotmail.com>
2015-11-07 00:08:58 +0100

    Lagt till test för kund. Hoppas att det blev rätt:P

--- DAL/Admin/DbKunderStub.cs
+++ DAL/Admin/DbKunderStub.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model.Nettbutikk;
+using DAL.Nettbutikk;
 
 namespace DAL.Admin
 {
@@ -16,7 +17,44 @@ namespace DAL.Admin
 
         public List<Kunde> getKunder()
         {
-            throw new NotImplementedException();
+            var liste = new List<Kunde>
+            {
+                new Kunde
+                {
+                    id = 1,
+                    fornavn = "Adrian",
+                    etternavn = "Westlund",
+                    adresse = "Årvollveien 60D",
+                    postnr = "0590",
+                    poststed = "Oslo",
+                    epost = "adrianwestlund@gmail.com",
+                    passordId = 1
+                },
+                new Kunde
+                {
+                    id = 2,
+                    fornavn = "Per",
+                    etternavn = "Andersson",
+                    adresse = "Svenskagatan 1",
+                    postnr = "0655",
+                    poststed = "Oslo",
+                    epost = "per@gmail.com",
+                    passordId = 2
+                },
+                new Kunde
+                {
+                    id = 3,
+                    fornavn = "Kalle",
+                    etternavn = "Anka",
+                    adresse = "Ankgatan 4C",
+                    postnr = "6542",
+                    poststed = "Ankeborg",
+                    epost = "ka@anka.com",
+                    passordId = 3
+                }
+            };
+
+            return liste;
         }
     }
 }
